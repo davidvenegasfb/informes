5367cb968e8782d12a8e469ac5bfb6d7fdf6a3ad
davidvenegasfb <davidvenegasfb@correo.ugr.es>
2021-06-14 12:43:09 +0200

    Please enter the commit message for your changes. Lines starting

--- informes/src/app/operarios-barredoras/operarios-barredoras.component.ts
+++ informes/src/app/operarios-barredoras/operarios-barredoras.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-operarios-barredoras',
@@ -14,7 +15,20 @@ export class OperariosBarredorasComponent implements OnInit {
   }
 
   pasarexcel(){
+    this.guardar();
     this.router.navigate(['/inicio']);
+  }
+
+  fileName = "ExcelResultado.xlsx"
+
+  guardar(){
+    let element = document.getElementById("body");
+    const ws: XLSX.WorkSheet = XLSX.utils.table_to_sheet(element);
+
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, "Maquinaria");
+
+    XLSX.writeFile(wb, this.fileName);
   }
 
 }
